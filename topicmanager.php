af1707e950396de9c470e171ac05266569ef235d
mambax7 <mambax7@gmail.com>
2021-02-12 04:16:16 -0500

    unnecessary double quotes

--- topicmanager.php
+++ topicmanager.php
@@ -254,7 +254,7 @@ if (Request::getString('submit', '', 'POST')) {
             $topicObject->loadFilters('update');
 
             //$sql = sprintf('UPDATE "%s" SET forum_id = "%u" WHERE topic_id = "%u"', $GLOBALS['xoopsDB']->prefix('newbb_posts'), $newforum, $topic_id);
-             $sql = sprintf("UPDATE %s SET forum_id = %u WHERE topic_id = %u", $GLOBALS['xoopsDB']->prefix('newbb_posts'), $newforum, $topic_id);
+             $sql = sprintf('UPDATE %s SET forum_id = %u WHERE topic_id = %u', $GLOBALS['xoopsDB']->prefix('newbb_posts'), $newforum, $topic_id);
             if (!$r = $GLOBALS['xoopsDB']->query($sql)) {
                 return false;
             }
